d9b190e49119d304a69771ea56b9fcc8df32f984
BitFlippa27 <32537389+BitFlippa27@users.noreply.github.com>
2022-07-28 22:13:09 -0400

    detect all faces

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./FaceRecognition.css";
+  
+const BoundingBox = ({box}) => {
+  console.log(box)
+  return (
+    <div className="bounding-box" style={{left: box.leftCol, top: box.topRow, right: box.rightCol, bottom: box.bottomRow}}></div>
+  )
+}
 
-const FaceRecognition = ({ imageUrl, box  }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
+  console.log(boxes)
   return (
     <div className="center ma">
       <div className="absolute mt2 ">
         <img id="input-image" className="br3"  src={imageUrl} alt="" width={"500px"} height="auto"/>
-        <div className="bounding-box" style={{top: box.topRow, bottom:box.bottomRow, left: box.leftCol, right: box.rightCol}}></div>
+        {boxes.map((box,i) => <BoundingBox box={box} key={i}/>)}
       </div>
     </div>
   )
